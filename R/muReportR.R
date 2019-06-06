624e3bd0b7e0a01795ebb194b6675804d093c331
Fabian Müller <fmueller@mpi-inf.mpg.de>
2019-06-06 10:47:06 -0700

    Depends —> Imports

---
+++ R/muReportR.R
@@ -0,0 +1,8 @@
+#' muReportR: Generating analysis reports in R.
+#'
+#' Generate HTML reports from R. Based on code from the RnBeads package.
+#'
+#' @import ggplot2
+#' @docType package
+#' @name muReportR
+NULL
